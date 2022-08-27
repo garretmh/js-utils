4b03cb37836684ef3db9e629c75523385f7541c2
Garret MH <git@garretmh.com>
2022-08-26 20:52:56 -0700

    update json module
    - export JSON ponyfil
    - export the global JSON object and its methods rather than wrappers

--- json/mod.ts
+++ json/mod.ts
@@ -7,23 +7,7 @@ export type json =
   | json[]
   | { [key: string]: json };
 
-/**
- * Converts a JavaScript Object Notation (JSON) string into an object.
- * @param text A valid JSON string.
- */
-export function parse(text: string): json;
-/**
- * Converts a JavaScript Object Notation (JSON) string into an object.
- * @param text A valid JSON string.
- * @param reviver A function that transforms the results. This function is called for each member of the object.
- * If a member contains nested objects, the nested objects are transformed before the parent object is.
- */
-export function parse(text: string, reviver: Reviver): unknown;
-export function parse(text: string, reviver?: Reviver): unknown {
-  return JSON.parse(text, reviver);
-}
-
-/** A value to stringify to JSON */
+/** A JSON stringify-able value. */
 export type jsonable =
   | string
   | number
@@ -34,47 +18,57 @@ export type jsonable =
   | { [key: string]: jsonable }
   | { toJSON(): jsonable };
 
-/**
- * Converts a JavaScript value to a JavaScript Object Notation (JSON) string.
- * @param value A JavaScript value, usually an object or array, to be converted.
- * @param space Adds indentation, white space, and line break characters to the return-value JSON text to make it easier to read.
- */
-export function stringify(
-  value: jsonable,
-  replacer?: never,
-  space?: string | number
-): string;
-/**
- * Converts a JavaScript value to a JavaScript Object Notation (JSON) string.
- * @param value A JavaScript value, usually an object or array, to be converted.
- * @param replacer Either a function that transforms the results, or an array of strings and numbers that acts as an approved list for selecting the object properties that will be stringified.
- * @param space Adds indentation, white space, and line break characters to the return-value JSON text to make it easier to read.
- */
-export function stringify(
-  value: unknown,
-  replacer: Replacer,
-  space?: string | number
-): string;
-export function stringify(
-  value: unknown,
-  replacer?: Replacer,
-  space?: string | number
-): string {
-  return JSON.stringify(value, replacer as any, space);
+interface JSON extends globalThis.JSON {
+  /**
+   * Converts a JavaScript Object Notation (JSON) string into an object.
+   * @param text A valid JSON string.
+   */
+  parse(text: string): json;
+  /**
+   * Converts a JavaScript Object Notation (JSON) string into an object.
+   * @param text A valid JSON string.
+   * @param reviver A function that transforms the results. This function is called for each member of the object.
+   * If a member contains nested objects, the nested objects are transformed before the parent object is.
+   */
+  parse(
+    text: string,
+    reviver?: (this: json, key: string, value: json) => unknown
+  ): unknown;
+  /**
+   * Converts a JavaScript value to a JavaScript Object Notation (JSON) string.
+   * @param value A JavaScript value, usually an object or array, to be converted.
+   * @param space Adds indentation, white space, and line break characters to the return-value JSON text to make it easier to read.
+   */
+  stringify(value: jsonable, replacer?: never, space?: string | number): string;
+  /**
+   * Converts a JavaScript value to a JavaScript Object Notation (JSON) string.
+   * @param value A JavaScript value, usually an object or array, to be converted.
+   * @param replacer A function that transforms the results.
+   * @param space Adds indentation, white space, and line break characters to the return-value JSON text to make it easier to read.
+   */
+  stringify(
+    value: unknown,
+    replacer?: (this: unknown, key: string, value: unknown) => unknown,
+    space?: string | number
+  ): string;
+  /**
+   * Converts a JavaScript value to a JavaScript Object Notation (JSON) string.
+   * @param value A JavaScript value, usually an object or array, to be converted.
+   * @param replacer An array of strings and numbers that acts as an approved list for selecting the object properties that will be stringified.
+   * @param space Adds indentation, white space, and line break characters to the return-value JSON text to make it easier to read.
+   */
+  stringify(
+    value: unknown,
+    replacer?: (number | string)[] | null,
+    space?: string | number
+  ): string;
 }
 
 /**
- * Either a function that transforms the results, or an array of strings and
- * numbers that acts as an approved list for selecting the object properties
- * that will be stringified.
+ * An intrinsic object that provides functions to convert JavaScript values to and from the JavaScript Object Notation (JSON) format.
  */
-type Replacer =
-  | ((this: unknown, key: string, value: unknown) => unknown)
-  | (number | string)[]
-  | null;
+export const JSON: JSON = window.JSON;
 
-/**
- * A function that transforms the results.
- * This function is called for each member of the object.
- */
-type Reviver = (this: json, key: string, value: json) => unknown;
+export const parse = JSON.parse;
+
+export const stringify = JSON.stringify;
